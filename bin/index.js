a998193eb0fc79955620b6560d422c75416a6f5d
SoorajSNBlaze333 <sooraj@skcript.com>
2020-10-11 13:58:29 +0530

    Minor fixes

--- bin/index.js
+++ bin/index.js
@@ -15,22 +15,28 @@ program.parse(process.argv);
 const imagePath = program.path;
 const { android, ios } = program;
 const currentDir = process.cwd();
+const dir = currentDir + '/favicons';
 
 console.log("Favic started!")
 if (imagePath) {
   const fileName = path.basename(imagePath, path.extname(imagePath));
   const fileType = path.extname(imagePath);
-  console.log("Path to the image", imagePath);
-  console.log("File Name", fileName);
-  console.log("File Type", fileType);
 
-  fs.readFile(imagePath, function(err, data) {
+  console.log("Creating favicons directory...")
+  fs.mkdir(dir, { recursive: true }, function(err, data) {
     if (err) return console.log(err);
-    fs.writeFileSync(currentDir+'/favicons/'+fileName+fileType, data, 'base64', function(err, data) {
+    console.log("Reading Image...")
+    fs.readFile(imagePath, function(err, data) {
       if (err) return console.log(err);
-      return console.log("File created under ./favicons")
+      fs.writeFileSync(dir+"/"+fileName+fileType, data, 'base64', function(err, data) {
+        if (err) return console.log(err);
+      })
+      return console.log("File created under "+dir)
     })
   })
+
+
+
 } else {
   console.log("Please provide the path to your Image");
   return;
